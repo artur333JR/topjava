d95675b76c16d510ef9334b10b343c1119a3af93
Artur Michurin <amichurin333@gmail.com>
2020-06-19 18:14:01 +0300

    complete HW01 without CRUD

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,8 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -16,9 +20,12 @@ public class MealServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
+        log.info("getAll");
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        List<MealTo> mealTos = MealsUtil.getToFilteredByDate(MealsUtil.mealStorage.getAll(), LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
+        request.setAttribute("mealTos", mealTos);
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+
+//        response.sendRedirect("meals.jsp");
     }
 }
